4cca4129b39e646065a5f8628310ff7bdf981d01
rannit012345 <rannit012345@gmail.com>
2021-07-20 13:25:55 +0530

    Lonngest substring without repeating characters

---
+++ DSAndAlgo/DSAndAlgo/SlidingWindow/LongestSubstringWithoutRepeatingChar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSAndAlgo.SlidingWindow
+{
+    class LongestSubstringWithoutRepeatingChar
+    {
+        /// <summary>
+        /// I/P - str = "pwwkew" | O/P - 3
+        /// I/P - str = "abcdabeabbeg" | O/P - 5
+        /// </summary>]
+        /// <param name="str"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int Find(string str)
+        {
+            int i = 0, j = 0, longest = 0;
+            var dict = new Dictionary<char, int>();
+
+            while (j < str.Length)
+            {
+                if (!dict.ContainsKey(str[j]))
+                {
+                    dict.Add(str[j], 1);
+                }
+                else
+                {
+                    dict[str[j]]++;
+                }
+
+                if (dict.Count == j - i + 1)
+                {
+                    longest = Math.Max(longest, j - i + 1);
+                    j++;
+                }
+                else if (dict.Count < j - i + 1)
+                {
+                    while (dict.Count < j - i + 1)
+                    {
+                        dict[str[i]]--;
+                        if (dict[str[i]] == 0)
+                        {
+                            dict.Remove(str[i]);
+                        }
+                        i++;
+                    }
+                    j++;
+                }
+            }
+            return longest;
+        }
+    }
+}
