3cea2f374d28d7812683c06ea684e77ee3fb12fb
rannit012345 <rannit012345@gmail.com>
2021-08-05 01:23:11 +0530

    MaximumAreaOfHistogram, MinimumElementInStackWithExtraSpace, MinimumElementInStackWithoutExtraSpace and RainWaterTrapping

---
+++ DSAndAlgo/DSAndAlgo/Stacks/MaximumAreaOfHistogram.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSAndAlgo.Stacks
+{
+    class MaximumAreaOfHistogram
+    {
+        /// <summary>
+        /// I/P - 6, 2, 5, 4, 5, 1, 6 | O/P- 12
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public double Area(int[] arr)
+        {
+            List<int> nslOfEachElement = NearestSmallerToLeft(arr);
+            List<int> nsrOfEachElement = NearestSmallerToRight(arr);
+
+            double max = double.MinValue;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                double area = arr[i] * (nsrOfEachElement[i] - nslOfEachElement[i] - 1);
+                Console.WriteLine("Area {0}", area);
+
+                max = Math.Max(max, area);
+            }
+
+            Console.WriteLine("Maximum Area {0}", max);
+            return max;
+        }
+
+        public List<int> NearestSmallerToLeft(int[] arr)
+        {
+            List<int> list = new List<int>();
+            Stack<KeyValuePair<int, int>> stack = new Stack<KeyValuePair<int, int>>();
+
+            int pseudoIndex = -1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (stack.Count == 0)
+                {
+                    list.Add(pseudoIndex);
+                }
+                else if (stack.Count > 0 && stack.Peek().Key < arr[i])
+                {
+                    list.Add(stack.Peek().Value);
+                }
+                else if (stack.Count > 0 && stack.Peek().Key >= arr[i])
+                {
+                    while (stack.Count > 0 && stack.Peek().Key >= arr[i])
+                    {
+                        stack.Pop();
+                    }
+
+                    if (stack.Count == 0)
+                    {
+                        list.Add(pseudoIndex);
+                    }
+                    else
+                    {
+                        list.Add(stack.Peek().Value);
+                    }
+                }
+
+                stack.Push(new KeyValuePair<int, int>(arr[i], i));
+            }
+
+            return list;
+        }
+
+        public List<int> NearestSmallerToRight(int[] arr)
+        {
+            List<int> list = new List<int>();
+            Stack<KeyValuePair<int, int>> stack = new Stack<KeyValuePair<int, int>>();
+
+            int pseudoIndex = arr.Length;
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if (stack.Count == 0)
+                {
+                    list.Add(pseudoIndex);
+                }
+                else if (stack.Count > 0 && stack.Peek().Key < arr[i])
+                {
+                    list.Add(stack.Peek().Value);
+                }
+                else if (stack.Count > 0 && stack.Peek().Key >= arr[i])
+                {
+                    while (stack.Count > 0 && stack.Peek().Key >= arr[i])
+                    {
+                        stack.Pop();
+                    }
+
+                    if (stack.Count == 0)
+                    {
+                        list.Add(pseudoIndex);
+                    }
+                    else
+                    {
+                        list.Add(stack.Peek().Value);
+                    }
+                }
+                stack.Push(new KeyValuePair<int, int>(arr[i], i));
+            }
+
+            //Since iterating from the end, just to maintain the order
+            list.Reverse();     
+
+            return list;
+        }
+    }
+
+}
---
+++ DSAndAlgo/DSAndAlgo/Stacks/MinimumElementInStackWithoutExtraSpace.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+
+namespace DSAndAlgo.Stacks
+{
+    class MinimumElementInStackWithoutExtraSpace
+    {
+        Stack<int> stack = new Stack<int>();
+        int me;
+
+        public int GetMinimum()
+        {
+            if (stack.Count == 0) return -1;
+
+            return me;
+        }
+
+        public void Push(int x)
+        {
+            if (stack.Count == 0)
+            {
+                stack.Push(x);
+                me = x;
+            }
+            else
+            {
+                if (x > me)
+                {
+                    stack.Push(x);
+                }
+                else if(x < me)
+                {
+                    stack.Push(2 * x - me);
+                    me = x;
+                }
+            }
+        }
+
+        public int Pop()
+        {
+            if (stack.Count == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                if (stack.Peek() >= me)
+                {
+                    return stack.Pop();
+                }
+                else if (stack.Peek() < me)
+                {
+                    int ans = me;
+                    me = 2 * me - stack.Peek();
+                    stack.Pop();
+                    return ans;
+                }
+
+                return -1;
+            }
+        }
+
+        public int Peek()
+        {
+            if (stack.Count == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                if (stack.Peek() >= me)
+                {
+                    return stack.Peek();
+                }
+                else if (stack.Peek() < me)
+                {
+                    return me;
+                }
+
+                return -1;
+            }
+        }
+
+    }
+
+    /*  Put this main
+
+        MinimumElementInStackWithoutExtraSpace min = new MinimumElementInStackWithoutExtraSpace();
+        min.Push(5);
+        min.Push(7);
+        min.Push(3);
+        Console.WriteLine("Pushed - 5, 7, 3");  
+        Console.WriteLine("Peek - {0}", min.Peek());
+        Console.WriteLine("Pop - {0}", min.Pop());
+        Console.WriteLine("Pop - {0}", min.Pop());
+        Console.WriteLine("Minimum Element - {0}", min.GetMinimum());
+
+    */
+}
